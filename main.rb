19602c29b6318d3f2a2f708d719c78a103255743
Konstantin Lynda <knlynda@gmail.com>
2015-03-12 11:20:12 +0200

    222

--- main.rb
+++ main.rb
@@ -1,5 +1,18 @@
 class Main
-  def foo
 
+  def initialize
+    @a = 1
+  end
+
+  def foo
+    puts 'foo'
+  end
+
+  def self.bar
+    puts 'bar'
+  end
+
+  def name
+    puts self.class.name
   end
 end
